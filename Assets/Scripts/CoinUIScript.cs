4fcdb4bc5aaf3de5d9d3ab2d97c8d7f8ee0d8b8e
Ah Heng <leesekheng@yahoo.com>
2017-01-19 19:41:10 +0800

    Able to modify the values of coins and rare resources.

---
+++ Assets/Scripts/CoinUIScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CoinUIScript : MonoBehaviour {
+    private HeroResourcesScript HeroCoins;
+    private Text theTextToChange;
+	// Use this for initialization
+	void Start () {
+        HeroCoins = GameObject.FindGameObjectWithTag("Player").GetComponent<HeroResourcesScript>();
+        theTextToChange = GetComponent<Text>();
+        ChangeCoinValue();
+	}
+	
+    void OnEnable()     // Registering to the MessageSystem
+    {
+        MessageSystem.instance.setListener("ModifyCoin", ChangeCoinValue);
+    }
+
+    void OnDisable()    // Registering to the MessageSystem
+    {
+        MessageSystem.instance.removeListener("ModifyCoin", ChangeCoinValue);
+    }
+
+    void ChangeCoinValue()
+    {
+        theTextToChange.text = HeroCoins.coinResources.ToString();
+    }
+}
---
+++ Assets/Scripts/HeroResourcesScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroResourcesScript : MonoBehaviour {
+    [Tooltip("Adjust the values of how much coins player has")]
+    public int coinResources = 1000;
+    [Tooltip("Adjust the values of how much rare resource player has")]
+    public int rareResources = 10;
+
+    void modifyCoins(int zeValue)
+    {
+        coinResources += zeValue;
+        MessageSystem.instance.triggerEventCall("ModifyCoin");
+    }
+    void modifyResource(int zeValue)
+    {
+        rareResources += zeValue;
+        MessageSystem.instance.triggerEventCall("ModifyResource");
+    }
+}
