1766d37d9fe42afc7f96e851854d7c214bde0f8a
Ah Heng <leesekheng@yahoo.com>
2017-01-19 20:24:25 +0800

    UI will go missing but won't come back.

--- Assets/Scripts/GameStateScript.cs
+++ Assets/Scripts/GameStateScript.cs
@@ -11,7 +11,7 @@ public class GameStateScript : MonoBehaviour {
         TOTAL_STATE,
     };
 
-    static public GAMESTATE currentGameState = GAMESTATE.PLAYING;
+    static private GAMESTATE currentGameState = GAMESTATE.PLAYING;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +20,54 @@ public class GameStateScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+	    // TODO: This is just keyboard input so that i can change UI elements at will only!
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            ChangeState(GAMESTATE.PLAYING);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            ChangeState(GAMESTATE.CHATTING);
+        // TODO: This is just keyboard input so that i can change UI elements at will only!
+    }
+
+    static public void ChangeState(GAMESTATE zeState)   // Changing the state then turning on the UI elements in that state
+    {
+        GameObject[] allUIElement = null;
+        switch (zeState)
+        {
+            case GAMESTATE.PLAYING:
+                allUIElement = GameObject.FindGameObjectsWithTag("GameplayUI");
+                turnOffUI_ElementInThatState(currentGameState);
+                currentGameState = zeState;
+             break;
+            case GAMESTATE.CHATTING:
+                allUIElement = GameObject.FindGameObjectsWithTag("ChattingUI");
+                turnOffUI_ElementInThatState(currentGameState);
+                currentGameState = zeState;
+                break;
+            default:
+                break;
+        }
+        if (allUIElement != null)
+            foreach (GameObject zeUI in allUIElement)   // Turning the UI Element on
+                zeUI.SetActive(true);
+  }
+    static private void turnOffUI_ElementInThatState(GAMESTATE zeState) // Turning off the UI elements in that state
+    {
+        GameObject[] allUIElement = null;
+        switch (zeState)
+        {
+            case GAMESTATE.PLAYING:
+                allUIElement = GameObject.FindGameObjectsWithTag("GameplayUI");
+                break;
+            case GAMESTATE.CHATTING:
+                allUIElement = GameObject.FindGameObjectsWithTag("ChattingUI");
+                break;
+            default:
+                break;
+        }
+        if (allUIElement != null)   // Need to check if the array is not null. then turn off the UI elements in that array
+            foreach (GameObject zeUI in allUIElement)   // Looping through the UI Element and set them to inactive
+            {
+                zeUI.SetActive(false);
+            }
+    }
 }
