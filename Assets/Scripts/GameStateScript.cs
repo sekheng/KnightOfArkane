b9ca945e5bda7d4db5d03201663a9215fe1ceba4
Ah Heng <leesekheng@yahoo.com>
2017-01-19 20:05:17 +0800

    Trying to figure out how to make state changes.

---
+++ Assets/Scripts/GameStateScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateScript : MonoBehaviour {
+    public enum GAMESTATE
+    {
+        PLAYING,
+        CHATTING,
+        LOSE,
+        WIN,
+        TOTAL_STATE,
+    };
+
+    static public GAMESTATE currentGameState = GAMESTATE.PLAYING;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
