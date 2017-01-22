ed5304a3723cfc252dc600f78ba2889fb3bee0b1
Ah Heng <leesekheng@yahoo.com>
2017-01-22 22:10:45 +0800

    Improving Health UI. But hero still can't recover from health.

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -5,16 +5,32 @@ public class HealthScript : MonoBehaviour {
     [Tooltip("This is to set the maximum health of the player only!")]
     public float max_health_ = 100.0f;
     private float health_;
+    [Tooltip("This is the regeneration of health per second!")]
+    public float Regeneration = 10;
+    private float timeToRegenerate = 0;
 
     void Start()
     {
         health_ = max_health_;
         MessageSystem.instance.triggerEventCall("ScaleHealthBar");  // Calling the event. Otherwise the healthbar will be gone!
     }
+
+    void Update()
+    {
+        timeToRegenerate += Time.deltaTime;
+        if (!Mathf.Approximately(max_health_, health_) && timeToRegenerate > 2)   // If it is more than 2 second and health is not the same as max health
+        {
+            health_ = Mathf.Min(max_health_, health_ + Regeneration * Time.deltaTime);
+        }
+    }
 	
     // Modify the health value! Returns true if health is more than 0 and return false if the health is equal or lesser than 0
     public bool modifyHealth(float zeValue)
     {
+        if (zeValue < Mathf.Epsilon)    // This means the player is taking damage!
+        {
+            timeToRegenerate = 0;   // Reset the regeneration counter!
+        }
         health_ += zeValue;
         bool whatToReturn = true;
         if (health_ <= Mathf.Epsilon)
--- Assets/Scripts/HealthUIScript.cs
+++ Assets/Scripts/HealthUIScript.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 /// <summary>
 /// This will scale the health bar of the UI according to player's health
 /// </summary>
 public class HealthUIScript : MonoBehaviour {
     private HealthScript playerHealth;  // Just get the player's health!
-    float originalScaleX;
+    private Image backgroundColorOfHealth;  // For changing of the color!
+
+    private float originalScaleX;
 	// Use this for initialization
 	void Start () {
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<HealthScript>(); // Getting the health of the player
+        backgroundColorOfHealth = GetComponent<Image>();
         originalScaleX = transform.localScale.x;    // get the scale of x!
         BeginScalingOfHealthBar();
 	}
@@ -27,18 +31,22 @@ public class HealthUIScript : MonoBehaviour {
     {
         // Get the original scale X then mulitply by the ratio of current health to max health. other values remains the same
         float percentageOfPlayerOverallHealth = playerHealth.getCurrentHealth() / playerHealth.max_health_;
+        //Debug.Log("Current Percentage Of Health=" + percentageOfPlayerOverallHealth);
         if (percentageOfPlayerOverallHealth <= 0.25f) // Checking whether is it below 25 percent, change to red colour!
         {
-
+            backgroundColorOfHealth.color = new Color32(237, 7, 7, 255);
+            //Debug.Log("Changed To Red");
         }
         else if (percentageOfPlayerOverallHealth <= 0.5f)   // less than 50 percent, change to orange colour!
         {
-
+            backgroundColorOfHealth.color = new Color32(226, 145, 4, 255);
+            //Debug.Log("Changed To Orange!");
         }
         else
         {
             // It shall always stay at green color!
-
+            backgroundColorOfHealth.color = new Color32(30, 255, 0, 255);
+            //Debug.Log("Changed To Green!");
         }
         float resultingScaleX = originalScaleX * percentageOfPlayerOverallHealth;
         //Debug.Log("Resulting Scale X:" + resultingScaleX);
