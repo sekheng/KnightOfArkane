e9954fe8ca7b61d1b9a96e47db061f0c6fa0ba53
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-01-25 10:54:17 +0800

    moving oof options in zy scene

---
+++ Assets/Scripts/ButtonMovenment.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonMovenment : MonoBehaviour {
+    private RectTransform btn_rt;
+	// Use this for initialization
+	void Start () {
+        btn_rt = GetComponent<RectTransform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void moveUP()
+    {
+        Vector2 temp = btn_rt.rect.position;
+        float coordY = temp.y + 216.5f;
+        btn_rt.transform.position += new Vector3(0,coordY,0);
+        //Debug.Log(coordY);
+        //temp.y += 216.5;
+    }
+
+    public void moveDown()
+    {
+        Vector2 temp = btn_rt.rect.position;
+        float coordY = temp.y - 216.5f;
+        btn_rt.transform.position -= new Vector3(0, coordY, 0);
+    }
+}
---
+++ Assets/Scripts/MenuButtonBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonBehaviour : MonoBehaviour {
+    private GameObject[] menuoptions;
+    private ButtonMovenment btn_move;
+    public string scrollDir;
+	// Use this for initialization
+	void Start () {
+        menuoptions = GameObject.FindGameObjectsWithTag("MenuButtons");
+        Debug.Log(menuoptions.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void pressUp()
+    {
+        foreach(GameObject go in menuoptions)
+        {
+            go.BroadcastMessage("moveUP");
+        }
+    }
+
+    public void pressDown()
+    {
+        foreach (GameObject go in menuoptions)
+        {
+            go.BroadcastMessage("moveDown");
+        }
+    }
+
+    //void moveUP()
+    //{
+
+    //}
+
+    //void moveDown()
+    //{
+
+    //}
+}
