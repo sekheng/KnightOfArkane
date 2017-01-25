44ec4cc8ff4ec0395f468408693dd91ab94ab362
Ah Heng <leesekheng@yahoo.com>
2017-01-25 15:40:18 +0800

    For some reason the order of the text mesh got messed up.

--- Assets/Scripts/LevelUIScript.cs
+++ Assets/Scripts/LevelUIScript.cs
@@ -5,11 +5,6 @@ public class LevelUIScript : MonoBehaviour {
     [Tooltip("Set the level of the character!")]
     public int m_Level = 20;
 
-    //[Tooltip("Set the Width and Height of the Level UI. The Position doesn't matter since it will be attach on top of the character")]
-    //public Rect PosOfText = new Rect(0, 0, 50, 50);
-
-    //private Texture2D m_Background, m_TextColor;
-
     [Tooltip("The Level UI to display")]
     public GameObject theLevelUI;
 
@@ -17,38 +12,36 @@ public class LevelUIScript : MonoBehaviour {
 
     void Start()
     {
-        //m_Background = new Texture2D(1, 1);
-        //m_TextColor = new Texture2D(1, 1);
-        //Color32[] allTheColor = new Color32[1];
-        //allTheColor[0] = new Color32(244, 160, 4, 255);
-        //m_TextColor.SetPixels32(allTheColor);   // Setting the text color
+        //characterSprite = GetComponent<SpriteRenderer>();
+        //GameObject zeCreatedLevelUI = Instantiate(theLevelUI);  //  Creating the Level UI
+        //TextMesh zeUIText = zeCreatedLevelUI.GetComponent<TextMesh>();
+        //zeUIText.text = "LVL:" + m_Level;
 
-        //allTheColor[0] = new Color32(0, 1, 143, 255);
-        //m_Background.SetPixels32(allTheColor);  // Setting the background color
+        //float ToConvertToWorldY = Camera.main.orthographicSize * 2;
+        //float ToConvertToWorldX = ToConvertToWorldY * Screen.width / Screen.height; // using ToConvertToWorldY * aspect ratio
+        //Debug.Log("ToConvertToWorldY:" + ToConvertToWorldY);
+        //Debug.Log("ToConvertToWorldX:" + ToConvertToWorldX);
 
-        characterSprite = GetComponent<SpriteRenderer>();
-        GameObject zeCreatedLevelUI = Instantiate(theLevelUI);  //  Creating the Level UI
-        TextMesh zeUIText = zeCreatedLevelUI.GetComponent<TextMesh>();
-        zeUIText.text = "LVL:" + m_Level;
+        //float unitWidthOfSprite = characterSprite.sprite.textureRect.width / characterSprite.sprite.pixelsPerUnit;
+        //float unitHeightOfSprite = characterSprite.sprite.textureRect.height / characterSprite.sprite.pixelsPerUnit;
 
-        float ToConvertToWorldY = Camera.main.orthographicSize * 2;
-        float ToConvertToWorldX = ToConvertToWorldY * Screen.width / Screen.height; // using ToConvertToWorldY * aspect ratio
-        Debug.Log("ToConvertToWorldY:" + ToConvertToWorldY);
-        Debug.Log("ToConvertToWorldX:" + ToConvertToWorldX);
+        //Vector2 EstimatedScaleOfCharacterForSprite = new Vector2(characterSprite.sprite.textureRect.width / unitWidthOfSprite, characterSprite.sprite.textureRect.height / unitHeightOfSprite);
+        //Debug.Log("Just the scale of sprite without it being scaled!:" + EstimatedScaleOfCharacterForSprite);
 
-        float unitWidthOfSprite = characterSprite.sprite.textureRect.width / characterSprite.sprite.pixelsPerUnit;
-        float unitHeightOfSprite = characterSprite.sprite.textureRect.height / characterSprite.sprite.pixelsPerUnit;
-
-        Vector2 EstimatedScaleOfCharacterForSprite = new Vector2(characterSprite.sprite.textureRect.width / unitWidthOfSprite, characterSprite.sprite.textureRect.height / unitHeightOfSprite);
-        Debug.Log("Just the scale of sprite without it being scaled!:" + EstimatedScaleOfCharacterForSprite);
-
-        //float offsetOfWord = zeUIText.text.Length * zeUIText.fontSize * 0.5f; // In order to make the words look centered to the character
-        float OffSetForCharacterY = (transform.localScale.y * 0.5f) + (characterSprite.sprite.textureRect.height * 0.5f);   // This isn't accurate. So need to convert again.
+        ////float offsetOfWord = zeUIText.text.Length * zeUIText.fontSize * 0.5f; // In order to make the words look centered to the character
+        //float OffSetForCharacterY = (transform.localScale.y * 0.5f) + (characterSprite.sprite.textureRect.height * 0.5f);   // This isn't accurate. So need to convert again.
         
         //Debug.Log("Offset for position from y:" + OffSetForCharacterY + ".Name:" + gameObject.name);
-        Debug.Log("Size Of Sprite:" + characterSprite.sprite.textureRect.size + ".Name:" + gameObject.name);
-        zeCreatedLevelUI.transform.position = new Vector3(transform.position.x, transform.position.y + OffSetForCharacterY, transform.position.z); // Making the UI to display above the character
-        zeCreatedLevelUI.transform.parent = transform;      // Making the UI's transform parent to be this
+        //Debug.Log("Size Of Sprite:" + characterSprite.sprite.textureRect.size + ".Name:" + gameObject.name);
+        //float totalPixel_height = characterSprite.sprite.textureRect.size.y * transform.localScale.y * 0.5f;   // Find out half of the total pixelheight
+        //Debug.Log("TotalPixel height:" + totalPixel_height);
+        //zeCreatedLevelUI.transform.position = new Vector3(transform.position.x, transform.position.y + ((totalPixel_height / (transform.localScale.y * transform.localScale.y)) * 0.5f), transform.position.z); // Making the UI to display above the character
+        //zeCreatedLevelUI.transform.SetParent(transform);      // Making the UI's transform parent to be this
+        //Debug.Log("Position of Level UI:" + zeCreatedLevelUI.transform.position);
+
+        // Solution #2
+        TextMesh zeChildMesh = transform.GetComponentInChildren<TextMesh>();
+        zeChildMesh.text = "LVL:" + m_Level;
     }
 	
     //void OnGUI()
