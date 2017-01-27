03b8b483c2300d47ce94497bd922a0a77a5b769e
Tristan Chia Shi-Jian <150513M@mymail.nyp.edu.sg>
2017-01-27 22:19:45 +0800

    Main Menu (Not Complete)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Text debugtxt;
+	// Use this for initialization
+	void Start () {
+		debugtxt.text = "Started";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+				if (Application.platform == RuntimePlatform.Android)
+				{
+					Application.Quit();
+				}
+		}
+	}
+
+	public void ClickedStart() {
+		debugtxt.text = "Start Up";
+
+	}
+
+	public void ClickedOptions() {
+		debugtxt.text = "Options";
+	}
+	
+	public void ClickedData() {
+		debugtxt.text = "Data";
+	}
+}
