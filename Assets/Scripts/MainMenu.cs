acb22760a5e08f4c7edd9aa16dc1ed1320d3f315
Tristan Chia Shi-Jian <150513M@mymail.nyp.edu.sg>
2017-01-29 04:03:29 +0800

    Data Transfer Screen (Does not function as there is not cloud servers)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,16 +3,44 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+	//public GameObject DataPanel;
+	public Canvas DataPanel2;
+	public bool DataOpen;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		DataOpen = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	}
 
 	public void ClickStart()
 	{
 		Application.LoadLevel("LoseScene");
+	}
+
+	public void ClickOptions()
+	{
+		Application.LoadLevel("ZhaoyuanScene");
+	}
+
+	public void ClickData()
+	{
+		if (DataOpen == false) 
+		{
+			DataOpen = true;
+			DataPanel2.enabled = true;
+		}
+	}
+	public void ClickCloseData()
+	{
+		if (DataOpen == true) 
+		{
+			DataOpen = false;
+			DataPanel2.enabled = false;
+		}
 	}
 }
