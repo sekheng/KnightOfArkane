3dc37d84ed290434189f5c310de50b4745c601c3
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-01-28 19:06:59 +0800

    pause menue now shows the correct UI based on the selected choice, did a scroll view for the item menu, starting to do the drag and drop.

---
+++ Assets/Scripts/zhaoyuan/DragHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
