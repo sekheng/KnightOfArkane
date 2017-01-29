d1abb77e85b83ed9d32adab26a560a8b96afdfb7
Ah Heng <leesekheng@yahoo.com>
2017-01-29 22:32:53 +0800

    successful updating of the item counts

---
+++ Assets/Scripts/SekHeng/UpdateItemCountScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// For now it will only update it's text according to the item counts!
+/// </summary>
+[RequireComponent(typeof(Text))]
+public class UpdateItemCountScript : MonoBehaviour {
+    private itemInformation theItemInform;
+    private Text ownText;
+
+	// Use this for initialization
+	void Start () {
+        ownText = GetComponent<Text>(); //Get the text
+       // We shall get the grandparent! (the item)
+        theItemInform = transform.parent.parent.gameObject.GetComponent<itemInformation>();
+    }
+	
+	// I would use observer pattern here but I don't have time for it!
+	void Update () {
+        ownText.text = theItemInform.item_count.ToString();
+	}
+}
