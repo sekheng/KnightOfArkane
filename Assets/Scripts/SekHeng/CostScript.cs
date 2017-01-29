04766ebe57423f757e8cfee40dcf03170260cc69
Ah Heng <leesekheng@yahoo.com>
2017-01-30 00:40:23 +0800

    Successful shop display stuff.

---
+++ Assets/Scripts/SekHeng/CostScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+///  This is for item's cost!
+///  This will also send it straight to the calculating logic
+/// </summary>
+public class CostScript : MonoBehaviour {
+    [Tooltip("Set the item's cost!")]
+    public int m_cost = 100;
+    [Tooltip("To send the cost and value to this gameobject!")]
+    public string m_objectName = "CountingCost";
+
+    private itemInformation ownItemStuff;
+    private GameObject calculateCostAndValue;   // To send it to the Buying UI!
+
+    void Start()
+    {
+        ownItemStuff = GetComponent<itemInformation>();
+        calculateCostAndValue = GameObject.Find(m_objectName);
+    }
+
+    public void sendCostAndAmount()
+    {
+        //Debug.Log("Sending Cost and Stuff");
+        calculateCostAndValue.BroadcastMessage("SetCost", m_cost);
+        calculateCostAndValue.BroadcastMessage("setMaxProducts", ownItemStuff.item_count);
+        calculateCostAndValue.BroadcastMessage("UpdateDisplay");
+    }
+}
