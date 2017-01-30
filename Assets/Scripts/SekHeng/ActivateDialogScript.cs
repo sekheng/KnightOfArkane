a2ecb8f5d5944a1499e70f31f69d88710f8bb364
Ah Heng <leesekheng@yahoo.com>
2017-01-30 16:18:23 +0800

    Doing it halfway...

---
+++ Assets/Scripts/SekHeng/ActivateDialogScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// For the buy button only!
+/// </summary>
+public class ActivateDialogScript : MonoBehaviour {
+    [Tooltip("Link it to the dialog box!")]
+    public CustomDialog dialogBox;
+    [Tooltip("Link it to the cost and calculation logic!")]
+    public GetCostAndQuantityScript costCalculatioLogic;
+
+    public void ActivateDialog()
+    {
+        if (costCalculatioLogic != null && dialogBox != null)
+        {
+            dialogBox.startDialog();
+        }
+    }
+}
