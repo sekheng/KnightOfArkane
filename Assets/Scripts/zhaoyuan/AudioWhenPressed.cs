078c5b3bc499ec47901ba6280f4ad47b7005e6e5
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-01-31 02:08:12 +0800

    sound feedback and vibrastion

---
+++ Assets/Scripts/zhaoyuan/AudioWhenPressed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AudioWhenPressed : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public Slider slider;
+
+    public void playSound()
+    {
+        if(audioSource != null)
+        {
+            audioSource.PlayOneShot(audioSource.clip, slider.value);
+        }
+    }
+}
