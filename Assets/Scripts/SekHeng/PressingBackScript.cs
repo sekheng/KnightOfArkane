9d260b48e96f6aa98850eff0423792ba36b2c033
Ah Heng <leesekheng@yahoo.com>
2017-01-31 10:46:49 +0800

    Giving the ability to pause menu scene and shop scene to go back to gameplay scene.

---
+++ Assets/Scripts/SekHeng/PressingBackScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This is just to deal with pressing the back button of the phone
+/// For now it will just be loading to another scene!
+/// </summary>
+public class PressingBackScript : MonoBehaviour {
+    public string sceneName = "SekHengScene";
+
+	// Update is called once per frame
+	void Update () {
+	    if (Application.platform == RuntimePlatform.Android && Input.GetKeyUp(KeyCode.Escape))  // if the player pressed the back button on the android phone, load the next scene!
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+	}
+}
